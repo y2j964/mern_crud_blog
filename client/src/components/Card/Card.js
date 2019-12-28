9f2f1c3c879289ebd0a9face8140176a299fbc3c
Justin Mooney <y2j964@aol.com>
2019-12-28 03:04:45 -0500

    Add separate slug for author
    The card should provide links to both the blog post and to the author's
    page; the data and props should reflect that.

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-function Card({ title, description, author, date }) {
+function Card({ title, description, author, date, authorSlug, postSlug }) {
   return (
     <article className="card">
       <div className="card__img-container">
@@ -10,14 +10,17 @@ function Card({ title, description, author, date }) {
       </div>
       <div className="card__body">
         <h2 className="card__title">
-          <Link to="/posts/7u1ijfjadn3" className="a-pseudo-wrap">
+          <Link to={`/posts/${postSlug}`} className="a-pseudo-wrap">
             {title}
           </Link>
         </h2>
         <p className="mb-4">{description}</p>
         <span className="card__small-print">
           Written by:{' '}
-          <Link to="/authors/isdfjnv9249" className="text-accent relative z-10">
+          <Link
+            to={`/authors/${authorSlug}`}
+            className="text-accent relative z-10"
+          >
             {author}
           </Link>
         </span>
---
+++ client/src/reducers/postReducer.js
@@ -0,0 +1,43 @@
+import { GET_POSTS, ADD_POST, DELETE_POST } from '../actions/types';
+
+const initialState = {
+  items: [
+    {
+      title: 'Welcome to MERN Crud Blog!',
+      description: 'This is a bunch of apocryphal rhubarb',
+      author: 'Dougie Jones',
+      date: 'Thursday 3:30PM',
+      authorSlug: 'dougie-jones',
+      postSlug: 'welcome-mern-crud-blog',
+    },
+    {
+      title: 'Me and My Tulpas',
+      description: 'A guide to living in the world of David Lynch',
+      author: 'Special Agent Dale Cooper',
+      date: 'Friday 3:30PM',
+      authorSlug: 'dale-cooper',
+      postSlug: 'me-and-my-tulpas',
+    },
+  ],
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case GET_POSTS:
+      return {
+        ...state,
+      };
+    case ADD_POST:
+      return {
+        ...state,
+        posts: [action.payload, ...state.posts],
+      };
+    case DELETE_POST:
+      return {
+        ...state,
+        posts: state.posts.filter(item => item._id !== action.payload),
+      };
+    default:
+      return state;
+  }
+}
