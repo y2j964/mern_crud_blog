c2a08d1cf95d4defdcc5c5dbac0da644d703e412
Justin Mooney <y2j964@aol.com>
2020-01-03 15:05:48 -0500

    Remove redux data state structure CardGroup map

--- client/src/components/Card/CardGroup.js
+++ client/src/components/Card/CardGroup.js
@@ -1,11 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import Card from './Card';
-import { connect } from 'react-redux';
+import { postsType } from './types';
 
 function CardGroup({ isEditable, posts }) {
-  const postFrags = posts.items.map(
-    ({ title, author, description, date, authorSlug, postSlug }) => (
+  const postFrags = posts.map(
+    ({ title, author, description, date, id, authorSlug, postSlug }) => (
       <Card
+        key={id}
         title={title}
         description={description}
         author={author}
@@ -19,49 +21,9 @@ function CardGroup({ isEditable, posts }) {
   return <div className="flex flex-col items-center">{postFrags}</div>;
 }
 
-const mapStateToProps = state => ({
-  posts: state.posts,
-});
+CardGroup.propTypes = {
+  isEditable: PropTypes.bool,
+  posts: postsType,
+};
 
-export default connect(mapStateToProps)(CardGroup);
-// import React, { useState, useEffect } from 'react';
-// import Card from './Card';
-// import { getPosts } from '../../actions/postActions';
-// import { connect } from 'react-redux';
-// import WithLoadingIndicator from '../WithLoadingIndicator';
-
-// function CardGroup({ posts, getPosts }) {
-//   const [isLoading, setIsLoading] = useState(false);
-
-//   useEffect(() => {
-//     setIsLoading(true);
-//     getPosts();
-//     setTimeout(() => {
-//       setIsLoading(false);
-//     }, 500);
-//   }, [getPosts]);
-
-//   const postFrags = posts.items.map(
-//     ({ title, author, description, date, authorSlug, postSlug }) => (
-//       <Card
-//         title={title}
-//         description={description}
-//         author={author}
-//         date={date}
-//         authorSlug={authorSlug}
-//         postSlug={postSlug}
-//       />
-//     )
-//   );
-//   return (
-//     <div className="flex flex-col items-center">
-//       <WithLoadingIndicator isLoading={isLoading} render={() => postFrags} />
-//     </div>
-//   );
-// }
-
-// const mapStateToProps = state => ({
-//   posts: state.posts,
-// });
-
-// export default connect(mapStateToProps, { getPosts })(CardGroup);
+export default CardGroup;
