b9f816e52bad72dcc7980ca80bf165fa3fa97748
Justin Mooney <y2j964@aol.com>
2020-01-03 15:20:59 -0500

    Add BodyScrollLock, FocusLock, and aria support

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,21 +1,58 @@
-import React from 'react';
-import Login from '../components/Login';
-import Register from '../components/Register';
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+// dialog should allow user to close with escape key
+
+import React, { useEffect, useRef } from 'react';
+import FocusLock, { AutoFocusInside } from 'react-focus-lock';
+import PropTypes from 'prop-types';
+import { disableBodyScroll, clearAllBodyScrollLocks } from 'body-scroll-lock';
 import Close from '../icons/Close';
 
-export default function Modal() {
+export default function Modal({ handleClose, children }) {
+  const modalRef = useRef();
+
+  useEffect(() => {
+    disableBodyScroll(modalRef.current);
+    return () => {
+      clearAllBodyScrollLocks();
+    };
+  }, []);
+
+  const handleKeyDown = e => {
+    const key = e.key || e.code;
+    if (key !== 'Escape') {
+      return;
+    }
+    handleClose();
+  };
+
   return (
-    <div className="absolutely-centered bg-alpha z-10">
+    <FocusLock returnFocus={{ preventScroll: false }}>
       <div
+        className="absolutely-centered bg-alpha z-10"
+        ref={modalRef}
+        onKeyDown={handleKeyDown}
+        aria-modal="true"
+        aria-labelledby="modalHeading"
         role="dialog"
-        className="max-w-sm sm:max-w-md md:max-w-lg w-full px-12 pt-6 pb-12 rounded bg-white relative"
       >
-        <button className="absolute right-0 top-0 p-4" aria-label="close modal">
-          <Close fill="black" />
-        </button>
-        <Login />
-        {/* <Register /> */}
+        <div className="max-w-sm sm:max-w-md md:max-w-lg w-full px-12 pt-6 pb-12 rounded bg-white relative">
+          <AutoFocusInside>
+            <button
+              className="absolute right-0 top-0 p-4"
+              aria-label="close modal"
+              onClick={handleClose}
+            >
+              <Close fill="black" />
+            </button>
+          </AutoFocusInside>
+          {children}
+        </div>
       </div>
-    </div>
+    </FocusLock>
   );
 }
+
+Modal.propTypes = {
+  handleClose: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+};
