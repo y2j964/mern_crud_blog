06bdd1b6345a1d97d1345bc5cdfc6d82e68dcc20
Justin Mooney <y2j964@aol.com>
2020-01-03 15:10:06 -0500

    Pass posts to CardGroup via render props
    This ensures that CardGroup is more reusable; we don't always want it to
    map throw all the posts. Therefore, it's better to pass in posts as a
    prop.

--- client/src/components/AllPosts.js
+++ client/src/components/AllPosts.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { getPosts } from '../actions/postActions';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { getPosts } from '../actions/postActions';
 import WithLoadingIndicator from './WithLoadingIndicator';
+import { postsType } from './Card/types';
 
-function AllPosts({ getPosts, children }) {
+function AllPosts({ posts, getPosts, children }) {
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
@@ -12,9 +14,53 @@ function AllPosts({ getPosts, children }) {
     setTimeout(() => {
       setIsLoading(false);
     }, 500);
-  }, [getPosts]);
+  }, [getPosts, posts]);
 
-  return <WithLoadingIndicator isLoading={isLoading} render={() => children} />;
+  return (
+    <WithLoadingIndicator
+      isLoading={isLoading}
+      render={() => children(posts)}
+    />
+  );
 }
 
-export default connect(null, { getPosts })(AllPosts);
+AllPosts.propTypes = {
+  posts: postsType,
+  getPosts: PropTypes.func.isRequired,
+  children: PropTypes.func,
+};
+
+const mapStateToProps = state => ({
+  posts: state.posts.items,
+});
+
+export default connect(mapStateToProps, { getPosts })(AllPosts);
+
+// import React, { useState, useEffect } from 'react';
+// import PropTypes from 'prop-types';
+// import { connect } from 'react-redux';
+// import { getPosts } from '../actions/postActions';
+// import WithLoadingIndicator from './WithLoadingIndicator';
+
+// function AllPosts({ getPosts, children }) {
+//   const [isLoading, setIsLoading] = useState(false);
+
+//   useEffect(() => {
+//     setIsLoading(true);
+//     getPosts();
+//     setTimeout(() => {
+//       setIsLoading(false);
+//     }, 500);
+//   }, [getPosts]);
+
+//   console.log('loader');
+
+//   return <WithLoadingIndicator isLoading={isLoading} render={() => children} />;
+// }
+
+// AllPosts.propTypes = {
+//   getPosts: PropTypes.func.isRequired,
+//   children: PropTypes.node,
+// };
+
+// export default connect(null, { getPosts })(AllPosts);
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,3 +1,37 @@
+// import React, { useEffect, useRef } from 'react';
+// import AllPosts from '../components/AllPosts';
+// import CardGroup from '../components/Card/CardGroup';
+// import { connect } from 'react-redux';
+
+// function Home({ posts }) {
+//   const ref = useRef();
+
+//   useEffect(() => {
+//     document.title = 'MERN Crud Blog';
+//     // focus h1 on route change to let screen reader know we changed route
+//     ref.current.focus();
+//   }, []);
+
+//   return (
+//     <main>
+//       <header className="py-8">
+//         <h1 tabIndex="-1" ref={ref} className="text-4xl text-center font-bold">
+//           Blog Posts
+//         </h1>
+//       </header>
+//       <AllPosts>
+//         <CardGroup posts={posts} />
+//       </AllPosts>
+//     </main>
+//   );
+// }
+
+// const mapStateToProps = state => ({
+//   posts: state.posts,
+// });
+
+// export default connect(mapStateToProps)(Home);
+
 import React, { useEffect, useRef } from 'react';
 import AllPosts from '../components/AllPosts';
 import CardGroup from '../components/Card/CardGroup';
@@ -18,9 +52,7 @@ export default function Home() {
           Blog Posts
         </h1>
       </header>
-      <AllPosts>
-        <CardGroup />
-      </AllPosts>
+      <AllPosts>{posts => <CardGroup posts={posts} />}</AllPosts>
     </main>
   );
 }
