9380883fa4cb9f154fc9d38c840e75f29a6edd48
Justin Mooney <y2j964@aol.com>
2020-01-16 17:41:04 -0500

    Render Dialog as child of Modal
    Since Dialog is seperate component, these components need to reflect
    that change. Otherwise, they have white background to sit against.

--- client/src/components/DeleteConfirmation.js
+++ client/src/components/DeleteConfirmation.js
@@ -1,33 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Modal from './Modal/Modal';
+import Dialog from './Modal/Dialog';
 
 function DeleteConfirmation({ onClose, id, deletePost }) {
   return (
     <Modal handleClose={onClose}>
-      <div className="mt-6 z-10">
-        <h2 className="font-bold text-2xl text-center mb-5" id="modalHeading">
-          Confirm Delete
-        </h2>
-        <p className="mb-5 text-center">
-          This action is irreversible. Are you sure you want to delete this
-          post?
-        </p>
-        <div className="flex justify-around">
-          <button className="accent-btn w-24 bg-gray-500" onClick={onClose}>
-            Cancel
-          </button>
-          <button
-            className="accent-btn w-24 bg-red-600"
-            onClick={() => {
-              deletePost(id);
-              onClose();
-            }}
-          >
-            Delete
-          </button>
+      <Dialog handleClose={onClose}>
+        <div className="mt-6 z-10">
+          <h2 className="font-bold text-2xl text-center mb-5" id="modalHeading">
+            Confirm Delete
+          </h2>
+          <p className="mb-5 text-center">
+            This action is irreversible. Are you sure you want to delete this
+            post?
+          </p>
+          <div className="flex justify-around">
+            <button className="accent-btn w-24 bg-gray-500" onClick={onClose}>
+              Cancel
+            </button>
+            <button
+              className="accent-btn w-24 bg-red-600"
+              onClick={() => {
+                deletePost(id);
+                onClose();
+              }}
+            >
+              Delete
+            </button>
+          </div>
         </div>
-      </div>
+      </Dialog>
     </Modal>
   );
 }
--- client/src/components/UserConfirmation.js
+++ client/src/components/UserConfirmation.js
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Modal from './Modal/Modal';
+import Dialog from './Modal/Dialog';
 
 export default function UserConfirmation({ message, callback }) {
   const [isHidden, setIsHidden] = useState();
@@ -21,26 +22,28 @@ export default function UserConfirmation({ message, callback }) {
 
   return (
     <Modal handleClose={() => closeModal(false)}>
-      <div className="mt-6 z-10">
-        <h2 className="font-bold text-2xl text-center mb-5" id="modalHeading">
-          Leaving Page
-        </h2>
-        <p className="mb-5 text-center">{message}</p>
-        <div className="flex justify-around">
-          <button
-            className="accent-btn w-24 bg-gray-300 text-black"
-            onClick={() => closeModal(false)}
-          >
-            No
-          </button>
-          <button
-            className="accent-btn w-24 bg-gray-300 text-black"
-            onClick={() => closeModal(true)}
-          >
-            Yes
-          </button>
+      <Dialog handleClose={() => closeModal(false)}>
+        <div className="mt-6 z-10">
+          <h2 className="font-bold text-2xl text-center mb-5" id="modalHeading">
+            Leaving Page
+          </h2>
+          <p className="mb-5 text-center">{message}</p>
+          <div className="flex justify-around">
+            <button
+              className="accent-btn w-24 bg-gray-300 text-black"
+              onClick={() => closeModal(false)}
+            >
+              No
+            </button>
+            <button
+              className="accent-btn w-24 bg-gray-300 text-black"
+              onClick={() => closeModal(true)}
+            >
+              Yes
+            </button>
+          </div>
         </div>
-      </div>
+      </Dialog>
     </Modal>
   );
 }
