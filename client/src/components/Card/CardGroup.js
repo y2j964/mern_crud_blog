9cc73f7469e88f7c2f67ce13db080d1921ffa948
Justin Mooney <y2j964@aol.com>
2020-01-22 14:42:01 -0500

    Add delete transition for Cards
    It's better UX to have a transition on delete.

--- client/src/components/Card/CardGroup.js
+++ client/src/components/Card/CardGroup.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import Card from './Card';
 import { postsType } from './types';
 
 function CardGroup({ isEditable, posts }) {
   const postFrags = posts.map(
     ({ title, author, description, date, _id, authorSlug, postSlug }) => (
-      <Card
-        key={_id}
-        title={title}
-        description={description}
-        author={author}
-        date={date}
-        _id={_id}
-        authorSlug={authorSlug}
-        postSlug={postSlug}
-        isEditable={isEditable}
-      />
+      <CSSTransition key={_id} timeout={300} classNames="fade-away">
+        <Card
+          title={title}
+          description={description}
+          author={author}
+          date={date}
+          _id={_id}
+          authorSlug={authorSlug}
+          postSlug={postSlug}
+          isEditable={isEditable}
+        />
+      </CSSTransition>
     )
   );
-  return <div className="flex flex-col items-center">{postFrags}</div>;
+  return (
+    <TransitionGroup className="flex flex-col items-center">
+      {postFrags}
+    </TransitionGroup>
+  );
 }
 
 CardGroup.propTypes = {
