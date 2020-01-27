afaa1620037ac8e0609b9c605de82970be100249
Justin Mooney <y2j964@aol.com>
2020-01-27 18:13:11 -0500

    Fix error caused by devtool when run outside browser
    When run in tests, the previous set up cause the tests to fail.

--- client/src/store.js
+++ client/src/store.js
@@ -1,17 +1,14 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import rootReducer from "./reducers/index";
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers/index';
 
 const initialState = {};
 const middleware = [thunk];
+const middlewareEnhancers = applyMiddleware(...middleware);
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const composedEnhancers = composeWithDevTools(middlewareEnhancers);
+
+const store = createStore(rootReducer, initialState, composedEnhancers);
 
 export default store;
