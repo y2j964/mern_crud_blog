46625f7ea21904eeca1214b03d05be0e17bf6f5d
Justin Mooney <y2j964@aol.com>
2020-01-31 20:53:12 -0500

    Cleanup lint warnings

--- client/src/components/PostsByAuthor.js
+++ client/src/components/PostsByAuthor.js
@@ -1,9 +1,9 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getPostsByAuthor } from '../selectors/postSelectors';
 import { postsType } from './Card/types';
 
+// eslint-disable-next-line no-unused-vars
 function PostsByAuthor({ posts, authorSlug, children }) {
   return children(posts);
 }
