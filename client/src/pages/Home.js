03ff200775ad09b6eedd7b3b367f19a9f8acf032
Justin Mooney <y2j964@aol.com>
2020-02-11 02:43:56 -0500

    Remove AllPosts and let Home handle fetching
    AllPosts is not a helpful abstraction. I think it is indeed sensible for
    Home to handle it's own fetching.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,42 +1,13 @@
-// import React, { useEffect, useRef } from 'react';
-// import AllPosts from '../components/AllPosts';
-// import CardGroup from '../components/Card/CardGroup';
-// import { connect } from 'react-redux';
-
-// function Home({ posts }) {
-//   const ref = useRef();
-
-//   useEffect(() => {
-//     document.title = 'MERN Crud Blog';
-//     // focus h1 on route change to let screen reader know we changed route
-//     ref.current.focus();
-//   }, []);
-
-//   return (
-//     <main>
-//       <header className="py-8">
-//         <h1 tabIndex="-1" ref={ref} className="text-4xl text-center font-bold">
-//           Blog Posts
-//         </h1>
-//       </header>
-//       <AllPosts>
-//         <CardGroup posts={posts} />
-//       </AllPosts>
-//     </main>
-//   );
-// }
-
-// const mapStateToProps = state => ({
-//   posts: state.posts,
-// });
-
-// export default connect(mapStateToProps)(Home);
-
 import React, { useEffect, useRef } from 'react';
-import AllPosts from '../components/AllPosts';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getPosts } from '../actions/postActions';
+import WithLoadingIndicator from '../components/WithLoadingIndicator';
+import { postsType } from '../components/Card/types';
 import CardGroup from '../components/Card/CardGroup';
 
-export default function Home() {
+// eslint-disable-next-line no-shadow
+function Home({ posts, isInitiallyFetched, isLoading, getPosts }) {
   const ref = useRef();
 
   useEffect(() => {
@@ -45,6 +16,12 @@ export default function Home() {
     ref.current.focus();
   }, []);
 
+  useEffect(() => {
+    if (!isInitiallyFetched) {
+      getPosts();
+    }
+  }, [isInitiallyFetched, getPosts]);
+
   return (
     <main>
       <header className="py-8">
@@ -52,7 +29,25 @@ export default function Home() {
           Blog Posts
         </h1>
       </header>
-      <AllPosts>{posts => <CardGroup posts={posts} />}</AllPosts>
+      <WithLoadingIndicator
+        isLoading={isLoading}
+        render={() => <CardGroup posts={posts} />}
+      />
     </main>
   );
 }
+
+Home.propTypes = {
+  posts: postsType,
+  isInitiallyFetched: PropTypes.bool.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+  getPosts: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = state => ({
+  isInitiallyFetched: state.posts.isInitiallyFetched,
+  posts: state.posts.items,
+  isLoading: state.communication.posts.isLoading,
+});
+
+export default connect(mapStateToProps, { getPosts })(Home);
