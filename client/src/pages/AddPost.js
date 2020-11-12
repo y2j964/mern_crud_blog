32aa96e718b5cb852b16f3b45d0f94cc43198930
Justin Mooney <y2j964@aol.com>
2020-11-12 02:15:19 -0500

    Fix heading layout inconsistency
    It is jarring to see the heading spaced lower for the AddPost page.

--- client/src/pages/AddPost.js
+++ client/src/pages/AddPost.js
@@ -20,7 +20,9 @@ function AddPost({ isAuthenticated, openLogin }) {
       <h1
         tabIndex="-1"
         ref={ref}
-        className="text-4xl pt-8 pb-2 text-center font-bold"
+        className={`text-4xl pt-8 text-center font-bold ${
+          !isAuthenticated ? 'pb-8' : 'pb-2'
+        }`}
       >
         Add Post
       </h1>
--- client/src/pages/EditPost.js
+++ client/src/pages/EditPost.js
@@ -21,7 +21,9 @@ function EditPost({ isAuthenticated, openLogin }) {
       <h1
         tabIndex="-1"
         ref={ref}
-        className="text-4xl pt-8 pb-2 text-center font-bold"
+        className={`text-4xl pt-8 text-center font-bold ${
+          !isAuthenticated ? 'pb-8' : 'pb-2'
+        }`}
       >
         Edit Post
       </h1>
